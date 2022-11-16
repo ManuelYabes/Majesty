8bb67b35972e5b0373260ec0384af93dae1d59e7
Manuel Yabes <imanuelyabesvernanda@gmail.com>
2022-11-16 19:02:34 +0700

    add side bar for mobile

--- template/user/ajax/search.php
+++ template/user/ajax/search.php
@@ -3,10 +3,10 @@
 require '../func/functions.php';
 
 $keyword  = $_GET["baju"];
-$result = query("SELECT nama,id_baju FROM daftar_baju WHERE nama LIKE '%$keyword%' LIMIT 0,5");
+$result = query("SELECT nama,id_baju,foto FROM daftar_baju WHERE nama LIKE '%$keyword%' LIMIT 0,5");
 
 ?>
 
 <?php foreach($result as $row): ?>
-    <li><a href="detail.php?id=<?= $row["id_baju"] ?>"><?= $row["nama"] ?></a></li>
+    <li class="flex items-center p-1 gap-2"><img class="w-8" src="../../media/img/<?= $row['foto'] ?>" alt=""><a class="text-base font-medium" href="detail.php?id=<?= $row["id_baju"] ?>"><?= $row["nama"] ?></a></li>
 <?php endforeach ?>
