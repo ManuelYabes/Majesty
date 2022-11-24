79399fc2ac3937c51a88b6ad1dbfb694a5e578d8
Manuel Yabes <imanuelyabesvernanda@gmail.com>
2022-11-24 09:03:35 +0700

    update databse

--- majesty.sql
+++ majesty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Nov 2022 pada 03.38
+-- Waktu pembuatan: 24 Nov 2022 pada 03.02
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 8.1.10
 
@@ -46,16 +46,17 @@ CREATE TABLE `daftar_baju` (
 --
 
 INSERT INTO `daftar_baju` (`id_baju`, `foto`, `nama`, `ukuran`, `pembayaran`, `stok`, `harga`, `berat`, `kondisi`, `kategori`, `deskripsi`) VALUES
-(2, 'image 22.png', 'Jas Hitam Poloss', 'S,M,L', 'BRI,BCA,COD,DANA,ALFA', 12, 50000, 120, 'Baru Belum Pernah Dicuci', 'Pernikahan', 'Keren'),
-(3, 'image 26.png', 'Baju Adat Keren', 'S,M,L', 'COD,BCA,BRI', 12, 50000, 120, 'Sudah Dicuci', 'Adat', 'Baju adat ini sangat tradisional'),
+(2, 'image 22.png', 'Jas Hitam Poloss', 'S,M,L', 'BRI,BCA,COD,DANA,ALFA', 9, 50000, 120, 'Baru Belum Pernah Dicuci', 'Pernikahan', 'Keren'),
+(3, 'image 26.png', 'Baju Adat Keren', 'S,M,L', 'COD,BCA,BRI', 11, 50000, 120, 'Sudah Dicuci', 'Adat', 'Baju adat ini sangat tradisional'),
 (4, 'image 27.png', 'Gaun Germelap', 'S,L', 'COD,ALFA', 12, 50000, 120, 'Sudah Dicuci', 'Pesta', 'gaun ini sangat mencolok colok'),
-(5, 'image 100.png', 'Gaun Emas', 'S', 'COD', 11, 50000, 120, 'Sudah Dicuci', 'Pernikahan', 'gaun ini bagus'),
+(5, 'image 100.png', 'Gaun Emas', 'S', 'COD', 4, 50000, 120, 'Sudah Dicuci', 'Pernikahan', 'gaun ini bagus'),
 (6, 'image 65.png', 'Gaun Coklat Terang Anggun', 'S', 'COD', 12, 50000, 300, 'Sudah Dicuci', 'Pernikahan', 'Pakaian ini bagus sekali'),
-(7, 'image 55.png', 'Gaun Putih Ramping', 'S', 'COD', 12, 50000, 300, 'Sudah Dicuci', 'Pernikahan', 'Pakaian ini bagus sekali'),
+(7, 'image 55.png', 'Gaun Putih Ramping', 'S', 'COD', 11, 50000, 300, 'Sudah Dicuci', 'Pernikahan', 'Pakaian ini bagus sekali'),
 (8, 'image 98.png', 'Gaun Putih Panjang', 'S', 'COD', 12, 50000, 300, 'Sudah Dicuci', 'Pernikahan', 'Pakaian ini bagus sekali'),
 (10, 'image 67.png', 'test', 'S', 'COD', 12, 50000, 300, 'Sudah Dicuci', 'Adat', 'Pakaian ini bagus sekali'),
 (16, '637d78947b8da.png', 'Dress Abad ke XI', 's,m,l,k', 'COD', 12, 1000, 300, 'Baru Belum Pernah Dicuci', 'Adat', 'pakaian ini berasal dr abad pertengahan'),
-(17, '637d83971d6c3.png', 'Gaun putih', 'S,M,L,K', 'COD', 12, 1000, 300, 'Baru Belum Pernah Dicuci', 'Pernikahan', 'Yang di foto itu Ratu Elizabeth Pas Muda');
+(17, '637d83971d6c3.png', 'Gaun putih', 'S,M,L,K', 'COD', 12, 1000, 300, 'Baru Belum Pernah Dicuci', 'Pernikahan', 'Yang di foto itu Ratu Elizabeth Pas Muda'),
+(18, '637daa7fe74c5.jpg', 'Temen anj', 'S,M,L,K', 'COD', 11, 1000, 300, 'Sudah Dicuci', 'Formal', 'tiada hari tanpa ngetrol');
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,8 @@ INSERT INTO `favorit` (`id`, `id_pengguna`, `id_baju`) VALUES
 (115, 1, 2),
 (169, 6, 13),
 (170, 6, 12),
-(178, 6, 6);
+(178, 6, 6),
+(186, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -111,20 +113,19 @@ CREATE TABLE `peminjam` (
 --
 
 INSERT INTO `peminjam` (`id`, `id_baju`, `id_pengguna`, `nama`, `nama_pengguna`, `ukuran`, `pembayaran`, `tanggal`, `tanggal_`, `code`, `total_harga`) VALUES
-(52, 5, 2, 'Gaun Emas', 'Tristan Eka Wirw', 'S', 'COD', '2022-11-22', '2022-11-24', 'b7acf9', '100000'),
-(53, 2, 2, 'Jas Hitam Polos Awikwok', 'Tristan Eka Wirw', 'S', 'BRI', '2022-11-22', '2022-11-25', '72e6b3', '150000'),
-(54, 7, 1, 'Gaun Putih Ramping', 'Yabessy', 'S', 'COD', '2022-11-22', '2022-11-24', 'fc5608', '100000'),
-(55, 6, 7, 'Gaun Coklat Terang Anggun', 'Arlan SGK', 'S', 'COD', '2022-11-24', '2022-11-30', '27637f', '300000'),
-(56, 1, 6, 'Gaun putih polos awikwok', 'omgHacker', 'S', 'BRI', '2022-11-22', '2022-11-24', '206372', '100000'),
-(57, 5, 6, 'Gaun Emas', 'omgHacker', 'S', 'COD', '2022-11-23', '2022-11-24', 'af4635', '50000'),
-(58, 5, 6, 'Gaun Emas', 'omgHacker', 'S', 'COD', '2022-11-22', '2022-11-23', '071b6d', '50000'),
-(59, 13, 6, 'Test', 'omgHacker', 's', 'test', '2022-11-22', '2022-11-24', '63b8ce', '244444'),
-(60, 5, 6, 'Gaun Emas', 'omgHacker', 'S', 'COD', '2022-11-22', '2022-11-23', 'd7b96f', '50000'),
-(61, 5, 6, 'Gaun Emas', 'omgHacker', 'S', 'COD', '2022-11-23', '2022-11-23', 'c747ce', '0'),
-(62, 1, 6, 'Gaun putih polos awikwok', 'omgHacker', 'S', 'BRI', '2022-11-22', '2022-11-22', '707b86', '0'),
-(63, 1, 6, 'Gaun putih polos awikwok', 'omgHacker', 'S', 'BRI', '2022-11-22', '2022-11-22', 'b39abc', '50000'),
-(64, 1, 6, 'Gaun putih polos awikwok', 'omgHacker', 'S', 'BRI', '2022-11-23', '2022-11-24', '5ae385', '50000'),
-(65, 13, 6, 'Test', 'omgHacker', 's', 'test', '2022-11-23', '2022-11-26', '63d66f', '366666');
+(66, 2, 6, 'Jas Hitam Poloss', 'omgHacker', 'M', 'COD', '2022-11-23', '2022-11-24', 'ed368c', '50000'),
+(67, 5, 6, 'Gaun Emas', 'omgHacker', 'S', 'COD', '2022-11-23', '2022-12-03', 'd73365', '500000'),
+(68, 18, 1, 'Temen anj', 'Yabessy', 'S', 'COD', '2022-11-23', '2022-11-23', '3c2787', '1000'),
+(69, 5, 1, 'Gaun Emas', 'Yabessy', 'S', 'COD', '2022-11-23', '2022-11-27', 'd7f394', '200000'),
+(70, 2, 1, 'Jas Hitam Poloss', 'Yabessy', 'M', 'BRI', '2022-11-23', '2022-11-24', '07bd36', '50000'),
+(71, 2, 1, 'Jas Hitam Poloss', 'Yabessy', 'M', 'BRI', '2022-11-23', '2022-11-24', '37337d', '50000'),
+(72, 3, 2, 'Baju Adat Keren', 'Tristan Eka Wirw', 'S', 'COD', '2022-11-23', '2022-11-24', 'cd7342', '50000'),
+(73, 5, 2, 'Gaun Emas', 'Tristan Eka Wirw', 'S', 'COD', '2022-11-24', '2022-11-26', '606db2', '100000'),
+(74, 5, 1, 'Gaun Emas', 'Yabessy', 'S', 'COD', '2022-11-24', '2022-11-24', '2c6187', '50000'),
+(75, 5, 1, 'Gaun Emas', 'Yabessy', 'S', 'COD', '2022-11-30', '2022-12-01', '4f1b93', '50000'),
+(76, 5, 1, 'Gaun Emas', 'Yabessy', 'S', 'COD', '2022-11-24', '2022-11-26', '23cbe4', '100000'),
+(77, 5, 1, 'Gaun Emas', '', 'S', 'COD', '2022-11-24', '2022-11-26', '71de64', '100000'),
+(78, 7, 1, 'Gaun Putih Ramping', '', 'S', 'COD', '2022-11-24', '2022-11-24', 'a5d7f3', '50000');
 
 -- --------------------------------------------------------
 
@@ -148,7 +149,7 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`id`, `nama`, `email`, `password`, `tanggal_lahir`, `jenis_kelamin`, `no_telepon`, `foto`) VALUES
-(1, 'Yabessy', 'imanuelyabesvernanda@gmail.com', '12', '2022-11-24', 'wanita', '83836610012', '637482d813061.jpg'),
+(1, 'Yabessy', 'imanuelyabesvernanda@gmail.com', '12', '2022-11-15', 'pria', '83836610012', '637db493a8f23.jpg'),
 (2, 'Tristan Eka Wirw', 'tristan@gmail.com', '31', NULL, '', '0', ''),
 (3, 'Tristan', 'tristanekawirwir@gmail.com', '31', NULL, '', '0', ''),
 (5, 'omgHacker', '', '$2y$10$XCSYpdYYMSLDB9RHtdOzmeSvMBqwB3lN37AMQCsl/D9LrA5fGIe/S', NULL, '', '', ''),
@@ -191,19 +192,19 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT untuk tabel `daftar_baju`
 --
 ALTER TABLE `daftar_baju`
-  MODIFY `id_baju` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_baju` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `favorit`
 --
 ALTER TABLE `favorit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=187;
 
 --
 -- AUTO_INCREMENT untuk tabel `peminjam`
 --
 ALTER TABLE `peminjam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengguna`
